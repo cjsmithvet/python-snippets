f70f19a06d5b89446a5d3b7f11d4445a51697e04
CJ Smith <cjsmithvet@gmail.com>
2020-01-28 19:28:30 -0800

    Better line by line reading, remove some debug spam

--- faultcode.py
+++ faultcode.py
@@ -15,11 +15,14 @@ def old_read_telnet(tn):
 def read_telnet(tn):
     while True:
         line = tn.read_until("\n", timeout=1)  # Read one line
-        print("read_telnet got: " + line)
-        sys.stdout.flush()
+        # print("read_telnet got: " + line)
+        # sys.stdout.flush()
         if (b"-->") in line:  # last line, no more read
-            break    
-    return line
+            break
+        else:
+            oldline = line
+    # print("read telnet returning: " + oldline)
+    return oldline
 
 def new_read_telnet(tn):
     a = ''
@@ -77,7 +80,7 @@ def print_display(tn):
 
 def emulate_drive_display(host):
     tn = telnetlib.Telnet()
-    tn.set_debuglevel(3)
+    # tn.set_debuglevel(3)
     try:
         tn.open(host, timeout=1)
     except:
@@ -98,7 +101,6 @@ def print_vbus_voltage(host):
     else:
         voltage = get_float_value(tn, 'VBUS.VALUE')
         print(host + " VBUS voltage: " + str(voltage) + " [Vdc]")
-        # The version of print imported from the future doesn't support flush
         sys.stdout.flush()
         tn.close()
 
@@ -107,33 +109,33 @@ def print_name(host):
     tn.open(host, timeout=1)
     response = get_string_value(tn, 'DRV.NAME')
     print(host + " name: " + response)
-    # The version of print imported from the future doesn't support flush
     sys.stdout.flush()
     tn.close()
 
 HOST = "10.0.0.166"
-# print("Capture Far")
-# print_name(HOST)
-# emulate_drive_display(HOST)
-# print("")
+print("Capture Far")
+print_name(HOST)
+emulate_drive_display(HOST)
+print("")
 
 HOST = "10.0.0.125"
 print("Capture Near")
 print_name(HOST)
-# emulate_drive_display(HOST)
+sys.stdout.flush()
+emulate_drive_display(HOST)
 print("")
 
 HOST = "10.0.0.120"
 print("Payout Far")
 print_name(HOST)
-# emulate_drive_display(HOST)
+emulate_drive_display(HOST)
 print("")
 
 HOST = "10.0.0.101"
 print("Payout Near")
 print_name(HOST)
-# emulate_drive_display(HOST)
+emulate_drive_display(HOST)
 print("")
-# print_vbus_voltage(HOST)
+print_vbus_voltage(HOST)
 
 
